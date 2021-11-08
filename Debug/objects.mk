e5808ff088dd4281a5e81fd045275b461a91692f
KrzysiekGL <webmaster@unexpectd.com>
2021-11-08 22:28:11 +0100

    Redo proj structure

--- Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -ldl -lGL -lGLEW -lglfw3 -lX11 -lpthread -lassimp -lCGL -lsoil2 -lBulletDynamics -lBulletCollision -lLinearMath
-
